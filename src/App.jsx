f47c109a7f7b646e94911208d04d9fd33cae10c0
ahmadkismael <ahmad.abualkhair@gmail.com>
2023-04-12 22:30:03 -0600

    adding usestate

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
+import { useState } from "react";
 import "./App.css";
 import SkillList from "./SkillList";
 
 function App() {
-  const skills = [
+  const [skills, setSkills] = useState([
     { name: "HTML", level: 5 },
     { name: "CSS", level: 4 },
     { name: "JavaScript", level: 4 },
     { name: "Python", level: 4 },
-  ];
+  ]);
+
+  // function handleAddSkills(){
+  //   setSkills((skills) => [...skills, 'adding']);
+  // }
+
+
   return (
     <div className="App">
       <h1 className="teal-text">React Dev Skills</h1>
